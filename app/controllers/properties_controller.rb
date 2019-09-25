ad395072754de86ffb631dca4235143dc22f5332
Mark Del Mar <mark@1000martians.com>
2019-09-25 15:54:04 -0400

    just stuff

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,4 +4,21 @@ class PropertiesController < ApplicationController
     @properties = Property.all
   end
 
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.create(
+      address: params[:address],
+      bedrooms: params[:bedrooms],
+      garage: params[:garage]
+    )
+    redirect_to properties_path(@property)
+  end
+
 end
