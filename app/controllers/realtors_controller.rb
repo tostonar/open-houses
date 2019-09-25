6fc53b22bec8fe1e3a4c5638d722bcfc9ae933f5
Andrea T <tostonar@gmail.com>
2019-09-25 17:22:18 -0400

    New Realtor Form

--- app/controllers/realtors_controller.rb
+++ app/controllers/realtors_controller.rb
@@ -6,6 +6,20 @@ class RealtorsController < ApplicationController
   def show
     @realtor = Realtor.find(params[:id])
   end
+
+  def new
+    @realtor = Realtor.new
+  end
+
+  def create
+    @realtor = Realtor.create(realtor_params)
+    redirect_to realtor_path(@realtor)
+  end
+
+  private
+  def realtor_params
+    params.require(:realtor).permit(:name, :brokerage, :years_experience)
+  end
   
   
 end
